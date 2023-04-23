78aa8db99f612a3ceae119b87c1047a46612d4b2
swarup3204 <swarupksms@gmail.com>
2023-04-23 20:16:26 +0530

    final code

--- A6/pingnetinfo.c
+++ A6/pingnetinfo.c
@@ -1,4 +1,87 @@
-#include "pingnetinfo.h"
+#ifndef PINGNETINFO_H
+#define PINGNETINFO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <strings.h>
+#include <unistd.h>
+#include <netinet/ip_icmp.h>
+#include <netinet/tcp.h>
+#include <netinet/ip.h>
+#include <netdb.h>
+#include <sys/time.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <poll.h>
+#include <limits.h>
+
+
+#define ECHO_REQUEST 1
+#define ECHO_REPLY 2
+#define TIME_EXCEEDED 3
+
+
+
+#define SUCCESS 0
+#define FAILURE -1
+
+
+// max packets to send to confirm a next hop in a route
+#define MAX_FIND_HOP_TRIES 8
+#define PORT_NO 0
+// port no of servers to connect to
+#define RECV_TIMEOUT 1
+// timeout for recvfrom() in seconds
+#define ICMP_PKT_SIZE 1024
+#define IP_PKT_SIZE 1200
+#define SLEEP_RATE 1
+#define MAX_HOPS 30
+#define N_MAX_IPS 20 
+// max number of ips in a route
+char *dest_ip;
+// a global variable to store the destination ip address
+int sockfd;
+// a global variable to store the socket file descriptor
+int udp_sockfd;
+// a global variable to store the udp socket file descriptor
+struct sockaddr_in *dest_addr_con;
+// a global variable to store the destination address in sockaddr_in format
+struct icmp_pkt
+{
+	struct icmphdr header;
+	char message[ICMP_PKT_SIZE - sizeof(struct icmphdr)];
+};
+// a structure to store the icmp packet with total size of 64 bytes
+struct ip_pkt
+{
+	struct iphdr header;
+	struct icmp_pkt icmp;
+	char message[IP_PKT_SIZE - sizeof(struct iphdr) - sizeof(struct icmphdr)];
+};
+// a structure to store the ip packet with total size of 128 bytes
+struct sizewise_rtt
+{
+	double rtt_64;
+	double rtt_80;
+};
+
+int num_probes;
+// a global variable to store the number of probes to be sent per link
+int time_diff;
+// a global variable to store the time difference between two consecutive probes
+
+struct icmp_pkt ROUTE_IPS[N_MAX_IPS];
+// to store the icmp packets echo replies received till now
+struct sizewise_rtt RTT_ARR[N_MAX_IPS];
+
+char *IP_ARR[N_MAX_IPS];
+
+#endif
 
 int parse_icmp_packet(struct icmphdr *icmp)
 {
